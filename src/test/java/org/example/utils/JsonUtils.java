c54c8445dc679f7e7071d4e5ab8e37d860748e2e
zyad_mohamed <zyadmohamed3711@gmail.com>
2026-02-10 00:55:25 +0200

    init commit

--- src/test/java/org/example/utils/JsonUtils.java
+++ src/test/java/org/example/utils/JsonUtils.java
@@ -12,15 +12,7 @@ public class JsonUtils {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    /**
-     * Reads a JSON file from the "src/test/resources/data" directory and maps it to a Java object.
-     *
-     * @param filePath The path to the JSON file relative to the "data" directory.
-     * @param valueType The class of the object to map the JSON to.
-     * @param <T> The type of the object.
-     * @return The mapped Java object.
-     * @throws IOException If the file cannot be read or mapped.
-     */
+
     public static <T> T readJsonFile(String filePath, Class<T> valueType) throws IOException {
         InputStream inputStream = getResourceAsStream("data/" + filePath);
         if (inputStream == null) {
@@ -29,15 +21,7 @@ public class JsonUtils {
         return objectMapper.readValue(inputStream, valueType);
     }
 
-    /**
-     * Reads a JSON file from the "src/test/resources/data" directory and maps it to a Java object with a specific TypeReference.
-     *
-     * @param filePath The path to the JSON file relative to the "data" directory.
-     * @param valueTypeRef The TypeReference of the object to map the JSON to.
-     * @param <T> The type of the object.
-     * @return The mapped Java object.
-     * @throws IOException If the file cannot be read or mapped.
-     */
+
     public static <T> T readJsonFile(String filePath, TypeReference<T> valueTypeRef) throws IOException {
         InputStream inputStream = getResourceAsStream("data/" + filePath);
         if (inputStream == null) {
@@ -46,13 +30,7 @@ public class JsonUtils {
         return objectMapper.readValue(inputStream, valueTypeRef);
     }
 
-    /**
-     * Writes a Java object to a JSON file in the "src/test/resources/data" directory.
-     *
-     * @param filePath The path to the JSON file relative to the "data" directory.
-     * @param data The Java object to write to the file.
-     * @throws IOException If the file cannot be written.
-     */
+
     public static void writeJsonFile(String filePath, Object data) throws IOException {
         File file = getResourceAsFile("data/" + filePath);
         objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, data);
@@ -65,7 +43,7 @@ public class JsonUtils {
     private static File getResourceAsFile(String resourceName) throws IOException {
         URL url = JsonUtils.class.getClassLoader().getResource(resourceName);
         if (url == null) {
-            // If resource doesn't exist, create the file path
+            // if resource doesn't exist, create the file path
             String path = "src/test/resources/" + resourceName;
             File file = new File(path);
             file.getParentFile().mkdirs();
